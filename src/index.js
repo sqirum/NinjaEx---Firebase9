fbea8cc4b83748ccfe98c055d1753a288af2bd02
sqirum <pauloslh94@gmail.com>
2022-10-15 15:09:57 +0100

    project setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDPGIKDOraFAdTGIWYvHHgfUPX1If3tU7Q",
+    authDomain: "fir-9-dojo-6fc10.firebaseapp.com",
+    projectId: "fir-9-dojo-6fc10",
+    storageBucket: "fir-9-dojo-6fc10.appspot.com",
+    messagingSenderId: "651821369165",
+    appId: "1:651821369165:web:d21beaaea053a39d839904"
+};
+
+initializeApp(firebaseConfig);
