699fae275adcc9a02ea79584c5c94bd1b9601dc3
sqirum <pauloslh94@gmail.com>
2022-10-17 19:37:27 +0100

    WIP

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import { initializeApp } from 'firebase/app';
+import {
+    CollectionReference,
+    getFirestore, collection, getDocs
+} from 'firebase/firestore'
 
 
 const firebaseConfig = {
@@ -10,4 +14,23 @@ const firebaseConfig = {
     appId: "1:651821369165:web:d21beaaea053a39d839904"
 };
 
+// Init firebase app
 initializeApp(firebaseConfig);
+
+// Init firebasae services
+const db = getFirestore(); 
+
+// Collection Reference
+const colRef = collection(db, 'books');
+
+// Get collection data
+
+getDocs(colRef)
+    .then( snapshot => {
+        let books = [];
+        snapshot.docs.forEach( doc => {
+            books.push({ ...doc.data(), id: doc.id });
+        })
+        console.log(books);
+    })
+    .catch(err => console.log(err));
